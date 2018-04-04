c5bbd7fd67b34df0d0e28b8e7368a21b1d20cd36
Daniel Schiffner <schiffner@sd.uni-frankfurt.de>
2018-04-04 23:17:08 +0200

    Moving files

--- lean-bundle/backend.py
+++ lean-bundle/backend.py
@@ -2,7 +2,7 @@ import os
 
 if os.getenv('LEAN_BACKEND', 'lmdb') == 'h5':
     print("Using H5 backend")
-    from h5 import *
+    from h5_backend import *
 else:
     print("Using LMDB backend")
-    from lm import *
+    from lmdb_backend import *
