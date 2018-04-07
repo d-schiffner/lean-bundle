c7231e30acd725ac14a3ff6b9843ed79d92dee33
Daniel Schiffner <schiffner@sd.uni-frankfurt.de>
2018-04-07 18:55:24 +0200

    Updated interaction_storage to use refs

--- lean_bundle/shelve_backend/interaction_storage.py
+++ lean_bundle/shelve_backend/interaction_storage.py
@@ -1,4 +1,4 @@
 
 def interaction_storage(fibers, data):
-    fibers.attrs['interaction_type'] = data[0]
-    fibers.attrs['interaction_target'] = data[1]
+    fibers.attrs['interaction_type'] = data[0].ref
+    fibers.attrs['interaction_target'] = data[1].ref
